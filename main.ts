8e3c32b1cccb58f7f2f055512aa5709902c7804e
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-03-31 13:20:23 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer"
+import chalk from "chalk";
+
+let todoList = [];
+let conditions = true;
+
+console.log(chalk.magenta.bold("\n \t ***==== Welcome to RABI todo's list ====*** \n"));
+
+while(conditions){
+ let addTask = await inquirer.prompt(
+ [
+ {
+ name: "task",
+ type: "input",
+ message:chalk.green("Enter your new task :")
+ }
+ ]
+ );
+ todoList.push(addTask.task)
+ console.log(`${addTask.task} Task added in Todo list successfully`);
+
+ let addMoreTask = await inquirer.prompt(
+ [
+ {
+ name: "addMore",
+ type: "confirm",
+ message: "Do you want to add more tasks?",
+ default: "false"
+ }
+ ]
+ );
+ conditions = addMoreTask.addMore
+}
+console.log("Your updated Todo list :" , todoList );
